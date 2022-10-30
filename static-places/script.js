685600f4ba7d3811dee323bb9e06869fe14d1be2
younglee-Choe <leele7111@naver.com>
2022-10-31 02:08:29 +0900

    set latitude and longitude

--- static-places/script.js
+++ static-places/script.js
@@ -13,8 +13,8 @@ function staticLoadPlaces() {
             name: 'Pokèmon',
             location: {
                 // decomment the following and add coordinates:
-                // lat: <your-latitude>,
-                // lng: <your-longitude>,
+                lat: 35.861237,
+                lng: 129.189807,
             },
         },
     ];
